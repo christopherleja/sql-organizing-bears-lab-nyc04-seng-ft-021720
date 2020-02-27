99c02741747d50b6a9229452fae402c57d5b6f2f
Christopher Leja <christopher.leja@gmail.com>
2020-02-27 02:30:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) AS MOST_FREQUENT from bears GROUP BY color ORDER BY COUNT(bears) DESC"
+  "SELECT COUNT(color) from bears GROUP BY color ORDER BY COUNT(color) DESC"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
